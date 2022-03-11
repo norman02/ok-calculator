dcc1b8cb5ed8837fdcb79b5ebe6449d0479a22f6
John N. Norman <normanj777@gmail.com>
2022-03-11 08:34:18 -0500

    implement multiply function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let calculator = {};
 
 calculator.add = (x, y) => x + y;
 
-calculator.subtract = (x,y) => x - y;
+calculator.subtract = (x, y) => x - y;
+
+calculator.multiply = (x, y) => x * y;
 
 module.exports = calculator;
